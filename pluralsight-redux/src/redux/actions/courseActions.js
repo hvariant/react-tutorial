9ef251f7044b7ebc8695db78c7d58a5e6d3f2734
Zhansong Li <lizhansong@hvariant.com>
2020-11-22 22:01:30 +1100

    set up manage course page

--- pluralsight-redux/src/redux/actions/courseActions.js
+++ pluralsight-redux/src/redux/actions/courseActions.js
@@ -1,19 +1,43 @@
-import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from "./actionTypes";
-import { getCourses } from "../../api/courseApi";
-
-export function createCourse(course) {
-  return { type: CREATE_COURSE, course };
-}
+import {
+  LOAD_COURSES_SUCCESS,
+  CREATE_COURSE_SUCCESS,
+  UPDATE_COURSE_SUCCESS,
+} from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
 
 export function loadCoursesSuccess(courses) {
   return { type: LOAD_COURSES_SUCCESS, courses };
 }
 
+export function createCourseSuccess(course) {
+  return { type: CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: UPDATE_COURSE_SUCCESS, course };
+}
+
 export function loadCourses() {
   return function (dispatch) {
-    return getCourses()
+    return courseApi
+      .getCourses()
       .then((courses) => {
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch) {
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((err) => {
         throw err;
--- pluralsight-redux/src/redux/reducers/courseReducer.js
+++ pluralsight-redux/src/redux/reducers/courseReducer.js
@@ -1,12 +1,20 @@
-import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from "../actions/actionTypes";
+import {
+  LOAD_COURSES_SUCCESS,
+  CREATE_COURSE_SUCCESS,
+  UPDATE_COURSE_SUCCESS,
+} from "../actions/actionTypes";
 import { initialState } from "./initialState";
 
 export function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
-    case CREATE_COURSE:
-      return [...state, { ...action.course }];
     case LOAD_COURSES_SUCCESS:
       return action.courses;
+    case UPDATE_COURSE_SUCCESS:
+      return state.map((course) =>
+        course.id === action.course.id ? action.course : course
+      );
+    case CREATE_COURSE_SUCCESS:
+      return [...state, { ...action.course }];
     default:
       return state;
   }
