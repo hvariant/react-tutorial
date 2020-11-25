1085320b3772b7da442fecb98e8dbea5d2b108b6
Zhansong Li <lizhansong@hvariant.com>
2020-11-25 21:44:38 +1100

    useState is pretty simple

--- pluralsight-react-hooks/pages/index.js
+++ pluralsight-react-hooks/pages/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputElement = () => {
-  return <input onChange={(e) => {}} placeholder="Enter some text" />;
+  const [inputText, setInputText] = useState("");
+  const [historyList, setHistoryList] = useState([]);
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          setInputText(e.target.value);
+          setHistoryList([...historyList, e.target.value]);
+        }}
+        placeholder="Enter some text"
+      />
+      <br />
+      {inputText}
+      <ul>
+        {historyList.map((rec) => {
+          return <p>{rec}</p>;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default InputElement;
