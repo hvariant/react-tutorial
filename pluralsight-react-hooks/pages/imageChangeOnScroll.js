23805aab7fce37c3ab7881bc8478913a28f799c9
Zhansong Li <lizhansong@hvariant.com>
2020-11-26 21:45:44 +1100

    add effect to scrolling

--- pluralsight-react-hooks/pages/imageChangeOnScroll.js
+++ pluralsight-react-hooks/pages/imageChangeOnScroll.js
@@ -1,58 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ImageToggleOnScroll from "../src/ImageToggleOnScroll";
 
 const ImageChangeOnScroll = () => {
+  const [currentDivId, setCurrentDivId] = useState(0);
+  const [mouseEventCount, setMouseEventCount] = useState(0);
+  useEffect(() => {
+    window.document.title = `# ${currentDivId}`;
+    console.log(`useEffect: setting title to ${currentDivId}`);
+  }, [currentDivId]);
   return (
     <div>
-      <ImageToggleOnScroll
-        primaryImage="static/bw/return-to-forever.png"
-        secondaryImage="static/return-to-forever.png"
-      />
-      <br />
-      <ImageToggleOnScroll
-        primaryImage="static/bw/romantic-warrior.png"
-        secondaryImage="static/romantic-warrior.png"
-      />
-      <br />
-      <ImageToggleOnScroll
-        primaryImage="static/bw/return-to-forever.png"
-        secondaryImage="static/return-to-forever.png"
-      />
-      <br />
-      <ImageToggleOnScroll
-        primaryImage="static/bw/romantic-warrior.png"
-        secondaryImage="static/romantic-warrior.png"
-      />
-      <br />
-      <ImageToggleOnScroll
-        primaryImage="static/bw/return-to-forever.png"
-        secondaryImage="static/return-to-forever.png"
-      />
-      <br />
-      <ImageToggleOnScroll
-        primaryImage="static/bw/romantic-warrior.png"
-        secondaryImage="static/romantic-warrior.png"
-      />
-      <br />
-      <ImageToggleOnScroll
-        primaryImage="static/bw/return-to-forever.png"
-        secondaryImage="static/return-to-forever.png"
-      />
-      <br />
-      <ImageToggleOnScroll
-        primaryImage="static/bw/romantic-warrior.png"
-        secondaryImage="static/romantic-warrior.png"
-      />
-      <br />
-      <ImageToggleOnScroll
-        primaryImage="static/bw/return-to-forever.png"
-        secondaryImage="static/return-to-forever.png"
-      />
-      <br />
-      <ImageToggleOnScroll
-        primaryImage="static/bw/romantic-warrior.png"
-        secondaryImage="static/romantic-warrior.png"
-      />
+      <span>mouseEventCount: {mouseEventCount}</span>
+      {[...Array(5).keys()].map((id) => {
+        return (
+          <div
+            key={id}
+            onMouseOver={() => {
+              setCurrentDivId(id);
+              setMouseEventCount(mouseEventCount + 1);
+              console.log(`onMouseOver:${id}`);
+            }}
+          >
+            <ImageToggleOnScroll
+              primaryImage="static/bw/return-to-forever.png"
+              secondaryImage="static/return-to-forever.png"
+            />
+            <br />
+            <ImageToggleOnScroll
+              primaryImage="static/bw/romantic-warrior.png"
+              secondaryImage="static/romantic-warrior.png"
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
