d26806a8bdd5f8ee73347e262da074d89c0bc16e
Zhansong Li <lizhansong@hvariant.com>
2020-11-26 21:31:39 +1100

    setIsLoading(true)

--- pluralsight-react-hooks/src/ImageToggleOnScroll.js
+++ pluralsight-react-hooks/src/ImageToggleOnScroll.js
@@ -8,21 +8,25 @@ const ImageToggleOnScroll = ({ primaryImage, secondaryImage }) => {
   const scrollHandler = () => {
     setInView(isInView());
   };
+  const [isLoading, setIsLoading] = useState(true);
 
   const imageRef = useRef(null);
   const [inView, setInView] = useState(false);
 
   useEffect(() => {
     setInView(isInView());
+    setIsLoading(false);
     window.addEventListener("scroll", scrollHandler);
     return () => {
       window.removeEventListener("scroll", scrollHandler);
     };
   }, []);
 
+  const transparentGif =
+    "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
   return (
     <img
-      src={inView ? secondaryImage : primaryImage}
+      src={isLoading ? transparentGif : inView ? secondaryImage : primaryImage}
       width="500"
       height="500"
       ref={imageRef}
